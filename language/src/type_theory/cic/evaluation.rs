b03ab3f4fde454bc459a32b3da56876e0af7bc9f
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2026-09-24 01:02:42 +0200

    reorganized code of try_iota_reduction

--- language/src/type_theory/cic/evaluation.rs
+++ language/src/type_theory/cic/evaluation.rs
@@ -74,92 +74,127 @@ pub fn one_step_reduction(
 /// `e_<Type>(params.., motive, case_1..case_k, instance)` whose `instance`
 /// is a concrete constructor application `ctor_i(params.., args..)`,
 /// computes to `case_i` applied to those args, with an inductive hypothesis.
+///
+/// A reference can be Inductive Definitions in the System Coq: Rules and
+/// Properties by Paulin
 fn try_iota_reduction(
     environment: &Environment<Cic>,
     term: &CicTerm,
 ) -> Option<CicTerm> {
-    let elim_name = match get_applied_function(term) {
-        Variable(name, NameKind::Const()) if name.starts_with("e_") => {
-            name.to_owned()
+    /// The inductive type corresponding to the eliminator used
+    fn eliminated_type(term: &CicTerm) -> Option<String> {
+        match get_applied_function(term) {
+            Variable(name, NameKind::Const()) => {
+                Some(name.strip_prefix("e_")?.to_string())
+            }
+            _ => None,
         }
-        _ => return None,
-    };
-    let type_name = elim_name.strip_prefix("e_")?.to_string();
+    }
+
+    /// Splits the used constructor's name and the supplied arguments
+    fn split_instance(instance: &CicTerm) -> Option<(String, Vec<CicTerm>)> {
+        match get_applied_function(instance) {
+            Variable(name, NameKind::Const()) => {
+                Some((name, application_args(instance)))
+            }
+            _ => None,
+        }
+    }
+
+    /// Returns argument types from constructor_type, reduced by `supplied_args`
+    /// by walking down `constructor_type`s Π chain
+    fn supplied_arg_types(
+        constructor_type: &CicTerm,
+        supplied_args: &[CicTerm],
+    ) -> Option<Vec<CicTerm>> {
+        let mut arg_types = vec![];
+        let mut remaining_type = constructor_type.to_owned();
+
+        for supplied in supplied_args {
+            match remaining_type {
+                Product(binder, domain, codomain) => {
+                    arg_types.push(*domain);
+                    remaining_type = substitute(&codomain, &binder, supplied);
+                }
+                // fewer Pi layers than supplied arguments: not a shape this
+                // rule understands
+                _ => return None,
+            }
+        }
+
+        Some(arg_types)
+    }
+
+    fn inductive_hypothesis(
+        type_name: &str,
+        leading_args: &[CicTerm],
+        param_count: usize,
+        occurrence: &CicTerm,
+        occurrence_type: &CicTerm,
+    ) -> Option<CicTerm> {
+        let occurrence_args = application_args(occurrence_type);
+        if occurrence_args.len() < param_count {
+            return None;
+        }
+
+        let mut hypothesis_args = leading_args.to_vec();
+        hypothesis_args.extend_from_slice(&occurrence_args[param_count..]);
+        // recursive occurance pushed as last arg for the eliminator
+        // ie the instance the eliminator is applied to
+        hypothesis_args.push(occurrence.to_owned());
+
+        Some(apply_arguments(
+            //TODO i need to review this im not sure why ud need to reuse
+            //the eliminator here instead of the motive
+            &Variable(format!("e_{}", type_name), NameKind::Const()),
+            hypothesis_args,
+        ))
+    }
+
+    let type_name = eliminated_type(term)?;
     let param_count = environment.get_inductive_param_count(&type_name)?;
     let constructors = environment.get_constructor_signatures(&type_name)?;
-    let constructor_count = constructors.len();
-    let args = application_args(term);
-    // #params + motive + #cases + instance
-    let fixed_args = param_count + 1 + constructor_count + 1;
 
+    // #params + motive + #cases + instance
+    let fixed_args = param_count + 1 + constructors.len() + 1;
+    let args = application_args(term);
     // partial application nothing to compute
     if args.len() < fixed_args {
         return None;
     }
 
-    let instance = args.last()?;
-    let instance_head = get_applied_function(instance);
-    let constructor_name = match &instance_head {
-        Variable(name, NameKind::Const()) => name.to_owned(),
-        // note: in case the instance isnt a constructor application
-        // recursion is fully stuck and have nothing to reduce
-        _ => return None,
-    };
-    let instance_args = application_args(instance);
+    let (constructor_name, instance_args) = split_instance(args.last()?)?;
     if instance_args.len() < param_count {
         return None;
     }
-
     let constructor_index = constructors
         .iter()
         .position(|(name, _)| name == &constructor_name)?;
-    let constructor_type = constructors[constructor_index].1.to_owned();
+    let arg_types =
+        supplied_arg_types(&constructors[constructor_index].1, &instance_args)?;
 
-    let case = args[param_count + 1 + constructor_index].to_owned();
-    // params + motive + every case
-    let leading_args = args[..param_count + 1 + constructor_count].to_vec();
+    // left params are already fixed by the eliminator's own arguments, only
+    // whats past them is fed to the case
     let own_args = &instance_args[param_count..];
+    let own_arg_types = &arg_types[param_count..];
+    // every argument except the instance, used for inductive hypothesis
+    let leading_args = &args[..param_count + 1 + constructors.len()];
 
-    // collect the types of own_args, reducing the type of the constructor
-    // by the instance arguments (mainly for left params instantiation)
-    let mut own_arg_types = vec![];
-    let mut remaining_type = constructor_type;
-    for supplied in instance_args.iter() {
-        match remaining_type {
-            Product(binder, domain, codomain) => {
-                own_arg_types.push((*domain).to_owned());
-                remaining_type = substitute(&codomain, &binder, supplied);
-            }
-            // fewer Pi layers than supplied arguments: not a shape this
-            // rule understands
-            _ => return None,
-        }
-    }
-    let own_arg_types = &own_arg_types[param_count..];
-
-    let mut reduced = case;
+    // take the case corresponding to instance and apply arguments from the constructor (+IH)
+    let mut reduced = args[param_count + 1 + constructor_index].to_owned();
     for (own_arg, own_arg_type) in own_args.iter().zip(own_arg_types.iter()) {
         reduced = Application(Box::new(reduced), Box::new(own_arg.to_owned()));
 
         // in case own_arg_type is this inductive type add an inductive hypothesis
         if is_instance_of(own_arg_type, &type_name) {
-            // this occurrence's own indices, ie whatever `Type(params.., i..)`
-            // it is an instance of, minus the (uniform) parameters
-            let occurrence_args = application_args(own_arg_type);
-            if occurrence_args.len() < param_count {
-                return None;
-            }
-            let occurrence_indices = &occurrence_args[param_count..];
-
-            let mut hypothesis_args = leading_args.clone();
-            hypothesis_args.extend(occurrence_indices.iter().cloned());
-            hypothesis_args.push(own_arg.to_owned());
-            let inductive_hypothesis = apply_arguments(
-                &Variable(elim_name.to_owned(), NameKind::Const()),
-                hypothesis_args,
-            );
-            reduced =
-                Application(Box::new(reduced), Box::new(inductive_hypothesis));
+            let hypothesis = inductive_hypothesis(
+                &type_name,
+                leading_args,
+                param_count,
+                own_arg,
+                own_arg_type,
+            )?;
+            reduced = Application(Box::new(reduced), Box::new(hypothesis));
         }
     }
 
